2b90703cdc1b064512c0c3b3aa2691c8db245e1c
Your Name <you@example.com>
2024-02-19 14:58:05 +0000

    m

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (Succes)
- * /
- * int main (void)
- * {
- * printf("Size of a char:%lu byte(s)\n";size of (char));
- * printf("size of an int:%lu byte(s)\n";size of (int));
- * printf("size of a long int:%lu byte (s)\n";size of(long long int));
- * printf("size of a floot:\n";size of (floot));
- * return(0);
- *}
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %1d byte(s)\n", sizeof(char));
+printf("Size of an int: %1d byte(s)\n", sizeof(int));
+printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %1d byte(s)\n", sizeof(float));
+return (0);
+}
