861d3de5f87bd6ad25085fb6dc29c86934c02b38
Your Name <you@example.com>
2024-02-19 14:24:33 +0000

    betty

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,13 @@
-#include <stdio.h>                                                                                                                  
-                                                                                                                                       
-/**                                                                                                                                 
-* main - Entry point                                                                                                               
-* Return: Always 0 (Success)                                                                                                       
-*/                                                                                                                                 
-int main(void)                                                                                                                      
-{                                                                                                                                   
-      printf("Size of a char: %lu byte(s)\n", sizeof(char));                                                                      
-      printf("Size of an int: %lu byte(s)\n", sizeof(int));                                                                       
-      printf("Size of a long int: %lu byte(s)\n", sizeof(long int));                                                              
-      printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));                                                     
-        printf("Size of a float: %lu byte(s)\n", sizeof(float));                                                                    
-       return (0);                                                                                                                 
-  }
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Succes)
+ * /
+ * int main (void)
+ * {
+ * printf("Size of a char:%lu byte(s)\n";size of (char));
+ * printf("size of an int:%lu byte(s)\n";size of (int));
+ * printf("size of a long int:%lu byte (s)\n";size of(long long int));
+ * printf("size of a floot:\n";size of (floot));
+ * return(0);
+ *}
