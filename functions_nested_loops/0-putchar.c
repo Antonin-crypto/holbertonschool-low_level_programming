b03400827b304c259ae2c9e433f2d21b5bb0bcbd
Your Name <you@example.com>
2024-02-21 12:28:17 +0000

    m

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
-#include <unistd.h>
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main- program should return 0
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letter;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	putchar(letter);
-
-	putchar('\n');
-}
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	return (0);
 }
