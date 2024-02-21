98eaaddfdec332214925baedd6a03a096e6837b1
Your Name <you@example.com>
2024-02-21 13:10:55 +0000

    m

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,20 @@
 #include "main.h"
+
 /**
- * main-prints the alphabet, in lowercase
- *
- * Return:Always 0
+ * print_alphabet - Entry point
+ * Return:0
  */
-int main(void)
+
+void print_alphabet(void)
 {
-	char letter;
+	char c = 'a';
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
 
+	}
 	_putchar('\n');
-	return (0);
+
 }
