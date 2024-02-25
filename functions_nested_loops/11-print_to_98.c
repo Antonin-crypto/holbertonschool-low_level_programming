0d19cd4dac9cb8123100637c4e28cf841deb9ee5
Your Name <you@example.com>
2024-02-25 11:46:23 +0000

    98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -5,38 +5,21 @@
   *
   * Return: Always 0.
   */
-void print_to_98(int n)
-{
-	if (n <= 98)
-	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				_putchar("%d", n);
-				_putchar("\n");
-				break;
-			}
-			else
-			{
-				_putchar("%d, ", n);
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				_putchar("%d", n);
-				_putchar("\n");
-				break;
-			}
-			else
-			{
-				_putchar("%d, ", n);
-			}
-		}
-	}
+void print_to_98(int n) {
+    if (n <= 98) {
+        for (int i = n; i <= 98; i++) {
+            printf("%d", i);
+            if (i < 98) {
+                printf(", ");
+            }
+        }
+    } else {
+        for (int i = n; i >= 98; i--) {
+            printf("%d", i);
+            if (i > 98) {
+                printf(", ");
+            }
+        }
+    }
+    putchar('\n');
 }
