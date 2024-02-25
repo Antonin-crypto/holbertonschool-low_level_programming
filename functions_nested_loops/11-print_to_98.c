3609877642b5570d77ac6149f30bf725c19cce23
Your Name <you@example.com>
2024-02-25 15:56:47 +0000

    98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,41 +1,41 @@
 #include "main.h"
 /**
-  * print_to_98 - Prints all natural numbers from n to 98
-  * @n: The number to start printing from
-  *
-  * Return: Always 0.
-  */
+* print_to_98 - Prints all natural numbers from n to 98
+* @n: The number to start printing from
+* Return: Always 0.
+*/
 void print_to_98(int n)
 {
-	while (n != 98)
+	if (n <= 98)
 	{
-		if (n < 0)
+		for (; n <= 98; n++)
 		{
-			_putchar('-');
-			n = -n;
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
-		int temp = n, count = 0;
-		while (temp > 0)
-		{
-			temp /= 10;
-			count++;
-		}
-		char str[count];
-		while (count > 0)
-		{
-			count--;
-			str[count] = n % 10 + '0';
-			n /= 10;
-		}
-		for (int i = 0; i < sizeof(str); i++)
-		{
-			_putchar(str[i]);
-		}
-		_putchar(',');
-		_putchar(' ');
-		n += (n < 98) ? 1 : -1;
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
 }
