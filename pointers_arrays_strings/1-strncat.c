f40a8c623b73e3e039003e87a69ad1e32bb35bbb
Your Name <you@example.com>
2024-02-29 16:51:37 +0000

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
+#include "2-strlen.c"
 /**
- *
- *
- *
- *
+ * _strncat - cat 2 string with n param
+ * @dest: string destinataire
+ * @src: string source
+ * @n: n bytes
+ * Return: dest with cat src
  */
 char *_strncat(char *dest, char *src, int n)
 {
-
-	size_t dest_len = strlen(dest);
-	size_t i;
+	int length = _strlen(dest);
+	int i;
 
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[length + i] = src[i];
 
-		dest[dest_len + i] = src[i];
-   
-	       	dest[dest_len + i] = '\0';
-		
-		return dest;
+	dest[length + i] = '\0';
+
+	return (dest);
 }
