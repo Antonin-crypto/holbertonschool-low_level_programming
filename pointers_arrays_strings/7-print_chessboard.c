bbd6a0e85bd49eaa5ff0f67c801bf0660baec06d
Your Name <you@example.com>
2024-03-05 09:43:19 +0000

    Chess is mental torture

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_chessboard - entry point
+ * @a: array
+ * Return: 0 (succes)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int ligne,  colonne;
+
+	for (ligne = 0; ligne < 8; ligne++)
+	{
+		for (colonne = 0; colonne < 8; colonne++)
+			_putchar(a[ligne][colonne]);
+		_putchar('\n');
+	}
+}
