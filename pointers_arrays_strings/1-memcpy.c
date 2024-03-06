2b4f3eefbfb563bdb24138c8d9f44194e87ddbea
Your Name <you@example.com>
2024-03-06 08:24:49 +0000

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * _memcpy - change memory with constant byte
- * @dest: pointer string destination
- * @src: pointer string source
- * @n: n bytes of memory pointed
- * Return: pointer to the memory dest
+ * _memcpy - Copies memory area
+ *
+ * @dest: where to copy the memory to
+ * @n: the number of bytes
+ * @src: the memory area to copy from
+ *
+ * Return: a pointer to @dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    char *p = dest;
-    unsigned int i = 0;
+	unsigned int i;
 
-    while (*src && i < n)
-    {
-        *p++ = src[i++];
-    }
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
-    return (dest);
+	return (dest);
 }
