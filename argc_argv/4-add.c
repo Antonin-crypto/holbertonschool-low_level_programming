f414e72b8aa9f22278d9a0a0aa53e2d58cf08590
Your Name <you@example.com>
2024-03-08 14:48:47 +0000

    To infinity and beyond

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
-#include "main.h"
-#include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
+#include "main.h"
+/**
+ * main - print sum of positive number
+ * @argc: number of arguments
+ * @argv: list of argument
+ * Return: 0 success
+ */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-for (i = 1; i < argc; i++)
-{
-int j;
-for (j = 0; argv[i][j]; j++)
-{
-if (!isdigit(argv[i][j]))
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[i]);
-}
-if (argc > 1)
-{
-printf("%d\n", sum);
-}
-else if (argc == 1)
-{
-printf("%d\n", argc - 1);
-}
-else
-{
-printf("0\n");
-}
-return (0);
+	int i, sum = 0;
+	unsigned int k;
+	char *e;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			e = argv[i];
+
+			for (k = 0; k < strlen(e); k++)
+			{
+				if (e[k] < 48 || e[k] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(e);
+			e++;
+		}
+
+		printf("%d\n", sum);
+	}
+	else
+		printf("0\n");
+
+	return (0);
 }
