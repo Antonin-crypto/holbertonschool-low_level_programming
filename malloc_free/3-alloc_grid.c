8a7404afae26f83d1e608f200c4d1cf02264b0b3
Your Name <you@example.com>
2024-03-11 14:56:53 +0000

    If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,10 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
+ * alloc_grid - allocate memorory a grid of integer
  * @width: width of the grid
  * @height: height of the grid
  *
- * Return: Nothing.
+ * Return: pointer to a 2d array
  */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, k, l, m;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(int *) * height);
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+
+		if (ptr[i] == NULL)
+		{
+			for (k = i; k >= 0; k--)
+				free(ptr[y]);
+			free(ptr);
+			return (NULL);
+		}
+	}
+
+	for (l = 0; l < height; l++)
+	{
+		for (m = 0; m < width; m++)
+			ptr[l][m] = 0;
+	}
+
+	return (ptr);
+}
