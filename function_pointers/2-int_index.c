f9f14a4c99c0d36be185c027fe94e531a11f3615
Your Name <you@example.com>
2024-03-18 11:22:19 +0000

    To hell with circumstances; I create opportunities

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,3 +9,20 @@
  * Return: 0 if false, something else otherwise.
  */
 int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
+	}
+	return (-1);
+}
