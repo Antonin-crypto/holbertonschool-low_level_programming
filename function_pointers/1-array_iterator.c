979338d9e36c48eea8ca53fabfa27cf06b19bb20
Your Name <you@example.com>
2024-03-18 10:25:00 +0000

    If you spend too much time thinking about a thing, you'll never get it done

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - array of integer from an function
+ * @array: array of integer
+ * @size: size of array
+ * @action: pointer to a function
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+	
+	for (i = 0; i < size ; i++)
+		action(array[i]);
+}
