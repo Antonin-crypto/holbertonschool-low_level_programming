cd4ed0676391d4e909a0ccadc274db871cab0874
Your Name <you@example.com>
2024-03-18 09:55:41 +0000

    What's my name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+
+/**
+ * print_name - a function prints a name
+ * @name: nome of function
+ * @f: pointer of function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,4 @@
-/*Prototype*/
+/*prototype*/
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
@@ -7,4 +7,3 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
