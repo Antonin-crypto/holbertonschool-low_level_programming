3614aefc940bc744445d2995db8a09f1a9eedf86
antonin-crypto <akamis3@gmail.com>
2024-04-02 08:40:04 +0000

    6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum of the n elements of a list
+ *
+ * @head: elements of list
+ * Return: an int
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+	const dlistint_t *p = head;
+
+	while (p != NULL)
+	{
+		sum += p->n;
+		p = p->next;
+	}
+	return (sum);
+}
