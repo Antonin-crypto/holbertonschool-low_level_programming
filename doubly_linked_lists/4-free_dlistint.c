c1b3e8102743fb5b1b4b9befcb9b1a288b705d40
antonin-crypto <akamis3@gmail.com>
2024-04-02 11:04:37 +0000

    modified file 4-free_dlistint

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,25 +1,23 @@
 #include "lists.h"
-
 /**
  * free_dlistint - Frees a doubly linked list
  * @head: Pointer to the head of the list
  *
- * Return: void 
+ * Return: void
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *first_next, *current = head;
+	dlistint_t *first_next, *current = head;
+	/*Traverse the list*/
+	while (current != NULL)
+	{
+		/*Store the next node's address*/
+		first_next = current->next;
 
-    /*Traverse the list*/
-    while (current != NULL)
-    {
-        /*Store the next node's address*/
-        first_next = current->next;
+		/*Free the current node*/
+		free(current);
 
-        /*Free the current node*/
-        free(current);
-
-        /*Move to the next node*/
-        current = first_next;
-    }
+		/*Move to the next node*/
+		current = first_next;
+	}
 }
