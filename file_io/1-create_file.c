3dfd711a0a7d88d5d36edf5024c3de60188bd8a1
antonin-crypto <akamis53@gmail.com>
2024-04-05 07:15:03 +0000

    checker

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,13 +8,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t file;
+	int fd;
 	size_t length = 0;
 
 	if (!filename)
 	{
 		return (-1);
 	}
+
+	fd = open(filename, O_CREAT| O_WRONLY | O_TRUNC, 0600);
 
 	if (text_content == NULL)
 	{
@@ -26,16 +28,16 @@ int create_file(const char *filename, char *text_content)
 		length++;
 	}
 
-	file = creat(filename, 0600);
-
-	if (file == -1)
+	if (fd == -1)
 	{
 		return (-1);
 	}
 
-	if (write(file, text_content, length) == -1)
+	if (write(fd, text_content, length) == -1)
 	{
 		return (-1);
 	}
+
+	close (fd);
 	return (1);
 }
